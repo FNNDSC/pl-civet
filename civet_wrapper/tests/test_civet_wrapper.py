ff4b3b5d249eb2ed8649a825f52bdc6b4f55f67c
Jennings Zhang <Jennings.Zhang@childrens.harvard.edu>
2020-04-20 20:39:06 -0400

    Simple tests

--- civet_wrapper/tests/test_civet_wrapper.py
+++ civet_wrapper/tests/test_civet_wrapper.py
@@ -4,13 +4,17 @@ from unittest import mock
 from civet_wrapper.civet_wrapper import Civet
 
 
+def occurrences(string, match):
+    return len(string.split(match)) - 1
+
+
 class CivetTests(TestCase):
     """
     Test Civet.
     """
     def setUp(self):
         self.app = Civet()
-
+    
     def test_run(self):
         """
         Test the run code.
@@ -25,9 +29,23 @@ class CivetTests(TestCase):
         # eg.
         # args.append('--custom-int')
         # args.append(10)
+        
+        args.append('-N3-distance')
+        args.append('200')
+        args.append('-resample-surfaces')
+        args.append('-thickness')
+        args.append('tlaplace:tfs:tlink')
+        args.append('30:20')
 
         options = self.app.parse_args(args)
-        self.app.run(options)
-
+        # self.app.run(options)
+        cli_options = self.app.assemble_cli(options)
         # write your own assertions
-        self.assertEqual(options.outputdir, 'outputdir')
+        
+        self.assertEqual(occurrences(cli_options, '-inputdir'), 1)
+        self.assertEqual(occurrences(cli_options, '-outputdir'), 1)
+        self.assertTrue('thickness tlaplace:tfs:tlink 30:20' in cli_options)
+        self.assertTrue('-N3-distance 200' in cli_options)
+        
+        after = cli_options[cli_options.index('-resample-surfaces'):]
+        self.assertTrue(len(after) == 0 or after.startswith(' -'))
